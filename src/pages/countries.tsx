25db15819c65965074fcc8d3562c7935add819c4
andres fernandez <andresfernandez.mdp@gmail.com>
2024-03-22 16:29:33 -0300

    page countries

---
+++ src/pages/countries.tsx
@@ -0,0 +1,9 @@
+import ContainerCountries from "../components/containerCountries/ContainerCountries";
+
+export default function Countries() {
+	return (
+		<>
+			<ContainerCountries />
+		</>
+	);
+}
