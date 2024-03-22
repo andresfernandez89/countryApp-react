d6e880077096e8015d72154f1a7efcc76732aaf1
andres fernandez <andresfernandez.mdp@gmail.com>
2024-03-22 18:03:58 -0300

    format all files with prettier

--- src/components/cardCountry/CardCountry.tsx
+++ src/components/cardCountry/CardCountry.tsx
@@ -1,7 +1,7 @@
 export default function CardCountry() {
   return (
     <>
-      <div className="w-30 md:w-40 bg-slate-950 shadow-xl rounded">
+      <div className="w-30 rounded bg-slate-950 shadow-xl md:w-40">
         <figure className="p-1.5">
           <img
             src="https://flagcdn.com/br.svg"
@@ -9,7 +9,7 @@ export default function CardCountry() {
             className="rounded-md"
           />
         </figure>
-        <div className="items-center text-center pb-2">
+        <div className="items-center pb-2 text-center">
           <h2 className="card-title justify-center">Argentina</h2>
           <p className="md:text-xs">South America</p>
         </div>
