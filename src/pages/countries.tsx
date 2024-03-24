0aa68b2ffb10c7db87e4974491a17b4460c2bce4
andres fernandez <andresfernandez.mdp@gmail.com>
2024-03-24 20:59:37 -0300

    users page

--- src/pages/countries.tsx
+++ src/pages/countries.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react';
+import { useState } from "react";
 import ContainerCountries from "../components/containerCountries/ContainerCountries";
-import { CountryModal } from '../components/modal/CountryModal';
-import { Portal } from '../components/modal/Portal';
+import { CountryModal } from "../components/modal/CountryModal";
+import { Portal } from "../components/modal/Portal";
 
 export default function Countries() {
   const [modal, setModal] = useState<boolean>(false);
 
-
   return (
     <>
       <ContainerCountries modalState={modal} setModalFn={setModal} />
-      <Portal modalState={modal} >
+      <Portal modalState={modal}>
         <CountryModal modalState={modal} setModalFn={setModal} />
       </Portal>
     </>
