61ace80abd1977364e498737c1e6642b4861a1c0
andres fernandez <andresfernandez.mdp@gmail.com>
2024-03-24 23:10:47 -0300

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
-
+import WorldSvg from "../assets/globe-world.svg";
 export const Home = () => {
   return (
-    <div className='grid grid-cols-2 gap-x-1.5 gap-y-2 rounded bg-neutral p-2 sm:grid-cols-3 md:grid-cols-4 md:gap-2 md:p-5  xl:grid-cols-6 2xl:grid-cols-7'>
-      Home
+    <div className="grid grid-cols-2 rounded bg-neutral p-2 md:p-5">
+      <section className="bg-base-100 pb-[25px] pl-[23px] pr-[25px] pt-[30px]">
+        La Tierra alberga 195 países soberanos, cada uno con su propia historia,
+        cultura, geografía y desafíos únicos. Desde microestados como Mónaco
+        hasta gigantes como Rusia, cada nación aporta algo diferente al tapiz
+        global. Algunos países son potencias económicas, mientras que otros
+        luchan por el desarrollo. Algunos disfrutan de una paz duradera,
+        mientras que otros enfrentan conflictos y agitación. La diversidad de
+        idiomas, religiones, tradiciones y formas de vida es asombrosa. En lugar
+        de intentar un resumen superficial, es mejor explorar paí
+      </section>
+      <img src={WorldSvg} />
     </div>
   );
 };
